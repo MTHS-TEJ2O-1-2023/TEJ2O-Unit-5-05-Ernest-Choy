fbad00a5398273491795a8925fed5b79d66095ca
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-11 13:23:54 -0400

    Type ver

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Ernest
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let neoPixelStrip: neopixel.Strip = null
+
+neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+// cut all colors
+neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neoPixelStrip.show()
+
+input.onButtonPressed(Button.A, function() {
+  neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+})
