c1adfdea953dbda03e9aa13f8242a5937cbb73cd
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-13 13:10:02 -0400

    fixed

--- main.ts
+++ main.ts
@@ -7,17 +7,32 @@
 
 let neoPixelStrip: neopixel.Strip = null
 
-neoPixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neoPixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
 
 // cut all colors
 neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neoPixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neoPixelStrip.show()
 
+//traffic light sequence
 input.onButtonPressed(Button.A, function() {
+  neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+  neoPixelStrip.show()
+  pause(1000)
   neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+  neoPixelStrip.show()
+  pause(1000)
   neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+  neoPixelStrip.show()
+  pause(1000)
+  basic.showIcon(IconNames.Happy)
+  pause(2000)
   neoPixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neoPixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neoPixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neoPixelStrip.show()
 })
