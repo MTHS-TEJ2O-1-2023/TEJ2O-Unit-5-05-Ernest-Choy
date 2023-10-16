61f042b6e2423fff75edf0d50592bcf6f2efcf7b
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-16 00:41:45 +0000

    Neopixel

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,33 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
 
+neoPixelStrip = neopixel.NeoPixel(pin16, 4)
 
-display.scroll("Hello, World!")
+# cut all colors
+neoPixelStrip[0] = (0, 0, 0)
+neoPixelStrip[1] = (0, 0, 0)
+neoPixelStrip[2] = (0, 0, 0)
+neoPixelStrip.show()
+
+# traffic light sequence
+if button_a.is_pressed():
+    neoPixelStrip[0] = (255, 0, 0)
+    neoPixelStrip.show()
+    sleep(1000)
+    neoPixelStrip[0] = (0, 0, 0)
+    neoPixelStrip[1] = (255, 255, 0)
+    neoPixelStrip.show()
+    sleep(1000)
+    neoPixelStrip[0] = (0, 0, 0)
+    neoPixelStrip[1] = (0, 0, 0)
+    neoPixelStrip[2] = (0, 255, 0)
+    neoPixelStrip.show()
+    sleep(1000)
+    display.show(Image.HAPPY)
+    sleep(2000)
+    neoPixelStrip[0] = (0, 0, 0)
+    neoPixelStrip[1] = (0, 0, 0)
+    neoPixelStrip[2] = (0, 0, 0)
+    neoPixelStrip.show()
