dfe8356d4bf818f1bba49dd6ccf3b05956f18d48
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-19 16:35:31 +0000

    PYVER

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,35 +7,40 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 import neopixel
 
+# veriables
+np = None
+
 display.clear()
 display.show(Image.HAPPY)
 
 # cut all colors
 np = neopixel.NeoPixel(pin16, 4)
-np[0] = (255, 0, 0)
-np[1] = (0, 255, 0)
-np[2] = (0, 0, 255)
+np[0] = (0, 0, 0)
+np[1] = (0, 0, 0)
+np[2] = (0, 0, 0)
 np.show()
 
 # traffic light sequence
-if button_a.is_pressed():
-    np[0] = (255, 0, 0)
-    np.show()
-    sleep(1000)
+while True:
+    if button_a.is_pressed():
+        np[0] = (255, 0, 0)
+        np.show()
+        sleep(1000)
 
-    # show Yellow
-    np[0] = (0, 0, 0)
-    np[1] = (255, 255, 0)
-    np.show()
-    sleep(1000)
+        # show Yellow
+        np[0] = (0, 0, 0)
+        np[1] = (255, 255, 0)
+        np.show()
+        sleep(1000)
 
-    # show green
-    np[1] = (0, 0, 0)
-    np[2] = (0, 255, 0)
-    np.show()
-    sleep(1000)
-    # Clear Led
-    display.show(Image.HAPPY)
-    sleep(2000)
-    np[2] = (0, 0, 0)
-    np.show()
+        # show green
+        np[1] = (0, 0, 0)
+        np[2] = (0, 255, 0)
+        np.show()
+        sleep(1000)
+
+        # Clear Led
+        display.show(Image.HAPPY)
+        sleep(2000)
+        np[2] = (0, 0, 0)
+        np.show()
